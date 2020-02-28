4e60bca7590df2013ede4116c63547ff06d42ccf
estebanalvarez <2264732790101@ingenieria.usac.edu.gt>
2020-02-27 21:19:34 -0600

    constantes para puertos

---
+++ config/ports.js
@@ -0,0 +1,12 @@
+const CLIENT_PORT = 3000;
+const SERVER_PORT = 4000;
+const RESTAURANT_PORT = 5000;
+const DELIVERY_PORT = 6000;
+
+
+module.exports = {
+    CLIENT_PORT,
+    SERVER_PORT,
+    RESTAURANT_PORT,
+    DELIVERY_PORT
+}
