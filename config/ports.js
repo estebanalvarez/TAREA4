31b399d7d274bb2cac2683af7b0dd7f46d99c757
estebanalvarez <2264732790101@ingenieria.usac.edu.gt>
2020-02-27 21:42:42 -0600

    puertos modificados

--- config/ports.js
+++ config/ports.js
@@ -2,11 +2,13 @@ const CLIENT_PORT = 3000;
 const SERVER_PORT = 4000;
 const RESTAURANT_PORT = 5000;
 const DELIVERY_PORT = 6000;
+const EBS_PORT = 8080;
 
 
 module.exports = {
     CLIENT_PORT,
     SERVER_PORT,
     RESTAURANT_PORT,
-    DELIVERY_PORT
+    DELIVERY_PORT,
+    EBS_PORT
 }
